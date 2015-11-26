3f5ec761f789d7dc80c14a47bbe4521b77f264f1
Charanjith Kunduru <kcharanjith@gmail.com>
2015-11-26 10:43:03 -0800

    Pushing changes via command line

--- c-grocery/MasterViewController.swift
+++ c-grocery/MasterViewController.swift
@@ -2,7 +2,7 @@
 //  MasterViewController.swift
 //  c-grocery
 //
-//  Created by charanjith on 7/19/15.
+//  Created by charanjith on 11/6/15.
 //  Copyright (c) 2015 crk. All rights reserved.
 //
 
